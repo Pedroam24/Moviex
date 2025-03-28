04692781cd5cdb9a23d2a21e7b073a261dc7e658
Pedro <pedroalmartins09@gmail.com>
2025-03-28 17:04:05 -0300

    Add files via upload

---
+++ MovieX/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MovieX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.Black;
+            this.txt1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt1.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt1.Location = new System.Drawing.Point(329, 113);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(142, 27);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.Black;
+            this.txt2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt2.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt2.Location = new System.Drawing.Point(329, 156);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(142, 23);
+            this.txt2.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Location = new System.Drawing.Point(329, 185);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(142, 24);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Login";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ MovieX/Form2.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MovieX
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt3
+            // 
+            this.txt3.BackColor = System.Drawing.Color.Black;
+            this.txt3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt3.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt3.Location = new System.Drawing.Point(328, 113);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(142, 27);
+            this.txt3.TabIndex = 1;
+            // 
+            // txt4
+            // 
+            this.txt4.BackColor = System.Drawing.Color.Black;
+            this.txt4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt4.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt4.Location = new System.Drawing.Point(328, 156);
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(142, 20);
+            this.txt4.TabIndex = 2;
+            // 
+            // txt6
+            // 
+            this.txt6.BackColor = System.Drawing.Color.Black;
+            this.txt6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt6.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt6.Location = new System.Drawing.Point(329, 234);
+            this.txt6.Multiline = true;
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(142, 20);
+            this.txt6.TabIndex = 3;
+            // 
+            // txt5
+            // 
+            this.txt5.BackColor = System.Drawing.Color.Black;
+            this.txt5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt5.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt5.Location = new System.Drawing.Point(329, 194);
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(142, 20);
+            this.txt5.TabIndex = 4;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Location = new System.Drawing.Point(329, 270);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(142, 24);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Create";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Button btn2;
+    }
+}
